36d971f317d88432cc9268dfa8be2b017e89e8fb
SebastianJL <johannes.lade@yahoo.com>
2023-09-13 18:23:07 +0200

    Add support for endianness in tipsy files.
    Also add tests.

--- tests/test_tipsy.py
+++ tests/test_tipsy.py
@@ -1,9 +1,10 @@
 from tipsy_io import Tipsy
 import numpy as np
+import pytest
 
 
 def test_save_load_cycle(tmpdir):
-    t = Tipsy.fromfile('tests/test_in.tipsy')
+    t = Tipsy.fromfile('tests/test_in_be.tipsy')
     outfile = tmpdir.join('test_out.tipsy')
     t.tofile(outfile)
     t_copy = t.fromfile(outfile)
@@ -12,3 +13,25 @@ def test_save_load_cycle(tmpdir):
     assert np.all(t.gas == t_copy.gas)
     assert np.all(t.dark == t_copy.dark)
     assert np.all(t.star == t_copy.star)
+
+
+def test_load_big_endian_auto():
+    t = Tipsy.fromfile('tests/test_in_be.tipsy')
+
+
+def test_load_little_endian_auto():
+    t = Tipsy.fromfile('tests/test_in_le.tipsy')
+
+
+def test_load_correct_byteorder():
+    t = Tipsy.fromfile('tests/test_in_be.tipsy', byteorder='>')
+
+
+def test_load_wrong_byteorder():
+    with pytest.raises(IOError, match=r"Couldn't open file with byteorder"):
+        t = Tipsy.fromfile('tests/test_in_be.tipsy', byteorder='<')
+
+
+def test_load_invalid_byteorder():
+    with pytest.raises(KeyError, match=r"byteorder must be one of (.+) or None"):
+        t = Tipsy.fromfile('tests/test_in_be.tipsy', byteorder=',')
--- tipsy_io/tipsy.py
+++ tipsy_io/tipsy.py
@@ -1,14 +1,17 @@
-from typing import List, Tuple
+from typing import Optional
 import numpy as np
+import numpy.typing as npt
 from dataclasses import dataclass, field
 
 header_type = np.dtype([('time', '>f8'), ('n', '>i4'), ('dims', '>i4'),
                        ('n_gas', '>i4'), ('n_dark', '>i4'), ('n_star', '>i4'), ('pad', '>i4')])
+_ALLOWED_BYTEORDERS = ('=', '>', '<')
+_ALLOWED_DIMS = (1, 2, 3)
 
 
 @dataclass
 class Tipsy:
-    header: header_type
+    header: npt.DTypeLike
     gas: np.array = field(repr=False)
     dark: np.array = field(repr=False)
     star: np.array = field(repr=False)
@@ -21,19 +24,72 @@ class Tipsy:
             self.star.tofile(ofile)
 
     @classmethod
-    def fromfile(cls, fname: str) -> "Tipsy":
-        with open(fname, 'rb') as tipsy:
-            header = np.fromfile(tipsy, dtype=header_type, count=1)[0]
-            n_dims = header['dims']
+    def fromfile(cls, fname: str, byteorder: Optional[str] = None) -> "Tipsy":
+        f"""Load tipsy file as Tipsy object.
+
+        Args:
+            fname: File name or path.
+            byteorder: Endianness of binary file. Allowed values are {_ALLOWED_BYTEORDERS} or None.
+                See https://numpy.org/doc/stable/reference/generated/numpy.dtype.byteorder.html for explanation.
+                If byte order is {None} then Tipsy tries to guess the byteorder in order of {_ALLOWED_BYTEORDERS}. 
+                Defaults to None.
+
+        Raises:
+            KeyError: Invalid byteorder.
+            IOError: Couldn't load file with specified byteorder.
+            IOError: Couldn't guess byteorder.
+
+        Returns:
+            Self
+        """
+        if byteorder not in _ALLOWED_BYTEORDERS + (None,):
+            raise KeyError(
+                f'byteorder must be one of {_ALLOWED_BYTEORDERS} or {None}')
+
+        with open(fname, 'rb') as binary_in:
+            # Explicitly defined byte order.
+            if byteorder is not None:
+                binary_in.seek(0)
+                header = np.fromfile(
+                    binary_in, count=1, dtype=header_type.newbyteorder(byteorder))[0]
+                n_dims = header['dims']
+                if n_dims not in _ALLOWED_DIMS:
+                    raise IOError(
+                        f"Couldn't open file with {byteorder = }: {fname}")
+            # Determine byte order from file.
+            else:
+                for bo in _ALLOWED_BYTEORDERS:
+                    # Return cursor to first byte.
+                    binary_in.seek(0)
+                    header = np.fromfile(
+                        binary_in, count=1, dtype=header_type.newbyteorder(bo))[0]
+                    n_dims = header['dims']
+                    print(f"{bo=}, {n_dims=}")
+                    if n_dims in _ALLOWED_DIMS:
+                        byteorder = bo
+                        break
+                else:
+                    raise IOError(
+                        f"Couldn't determine correct byteorder of file {fname}.")
+
+            # Define dtypes.
             gas_type = np.dtype([('mass', '>f4'), ('r', '>f4', (n_dims,)), ('v', '>f4', (n_dims,)),
                                  ('rho', '>f4'), ('temp', '>f4'), ('hsmooth', '>f4'), ('metals', '>f4'), ('phi', '>f4')])
             dark_type = np.dtype([('mass', '>f4'), ('r', '>f4', (n_dims,)), ('v', '>f4', (n_dims,)),
                                   ('eps', '>f4'), ('phi', '>f4')])
             star_type = np.dtype([('mass', '>f4'), ('r', '>f4', (n_dims,)), ('v', '>f4', (n_dims,)),
                                   ('metals', '>f4'), ('tform', '>f4'), ('eps', '>f4'), ('phi', '>f4')])
-            gas = np.fromfile(tipsy, dtype=gas_type, count=header['n_gas'])
-            dark = np.fromfile(tipsy, dtype=dark_type,
+
+            # Change byteorder of dtypes.
+            gas_type = gas_type.newbyteorder(byteorder)
+            dark_type = dark_type.newbyteorder(byteorder)
+            star_type = star_type.newbyteorder(byteorder)
+
+            # Load data
+            gas = np.fromfile(binary_in, dtype=gas_type, count=header['n_gas'])
+            dark = np.fromfile(binary_in, dtype=dark_type,
                                count=header['n_dark'])
-            star = np.fromfile(tipsy, dtype=star_type,
+            star = np.fromfile(binary_in, dtype=star_type,
                                count=header['n_star'])
+
             return cls(header, gas, dark, star)
